475d2cb016bab82805d5743d99d4a5d905ad6778
Ivan Zhukov <36888051+vanwise@users.noreply.github.com>
2025-12-15 23:58:07 +0600

    feat: add tests for more cases

--- src/test/kotlin/CalcCommissionTest.kt
+++ src/test/kotlin/CalcCommissionTest.kt
@@ -4,8 +4,20 @@ import org.junit.Test
 class CalcCommissionTest {
     @Test
     fun calcCommissionShouldReturnZero() {
-        val result = calcCommission(200, "vk-pay", 300)
+        val result = calcCommission(200, "vk-pay", 0)
         assertEquals(0.0, result, 0.0001)
+    }
+
+    @Test
+    fun calcCommissionShouldReturnCorrectCommissionsWithVisa() {
+        val result = calcCommission(5200, "visa", 0)
+        assertEquals(39.0, result, 0.0001)
+    }
+
+    @Test
+    fun calcCommissionShouldReturnCorrectCommissionsWithMir() {
+        val result = calcCommission(5200, "mir", 0)
+        assertEquals(39.0, result, 0.0001)
     }
 
     @Test
@@ -15,12 +27,32 @@ class CalcCommissionTest {
     }
 
     @Test
-    fun calcVisaCommissionShouldReturnCorrectCommission() {
-        val result = calcVisaCommission(5200)
-        assertEquals(39.0, result, 0.0001)
+    fun calcMastercardCommissionShouldZero() {
+        val result = calcCommission(50_000, "mastercard", 0)
+        assertEquals(0.0, result, 0.0001)
     }
 
     @Test
-    fun calcMastercardCommission() {
+    fun calcMastercardCommissionShouldZeroWithMonthAmount() {
+        val result = calcCommission(200, "mastercard", 300)
+        assertEquals(0.0, result, 0.0001)
+    }
+
+    @Test
+    fun calcMastercardCommissionShouldCommissionFromMiddleAmountAndMonthAmount() {
+        val result = calcCommission(60_000, "maestro", 20_000)
+        assertEquals(380.0, result, 0.0001)
+    }
+
+    @Test
+    fun calcMastercardCommissionShouldReturnCommissionFromLowAmount() {
+        val result = calcCommission(200, "mastercard", 0)
+        assertEquals(21.2, result, 0.0001)
+    }
+
+    @Test
+    fun calcMastercardCommissionShouldReturnCommissionFromHighAmount() {
+        val result = calcCommission(76_000, "maestro", 0)
+        assertEquals(476.0, result, 0.0001)
     }
 }
